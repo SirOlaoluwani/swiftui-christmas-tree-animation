1611835ca76f7283380809ca8d7beb1d4fc0f5b4
Olaoluwani Onafowope <olaoluwa@getbumpa.com>
2023-01-01 15:48:30 +0100

    Initial Commit

---
+++ ChrismasTree/ChrismasTreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChrismasTreeApp.swift
+//  ChrismasTree
+//
+//  Created by Olaoluwani Onafowope on 01/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChrismasTreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
